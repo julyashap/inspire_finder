42a4612d674f0767b39d9e4461d6c36f5b7b7c49
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-02 14:57:25 +0300

    Add Item and Like models

--- recommendations/models.py
+++ recommendations/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from config import settings
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'item {self.name}'
+
+    class Meta:
+        verbose_name = 'элемент'
+        verbose_name_plural = 'элементы'
+
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, verbose_name='элемент')
+
+    def __str__(self):
+        return f'like from {self.user} to {self.item}'
+
+    class Meta:
+        verbose_name = 'лайк'
+        verbose_name_plural = 'лайки'

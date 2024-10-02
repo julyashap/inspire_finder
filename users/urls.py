b7fbbd3eb624a4083b28a40cee0ebc1fcb2d9c56
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-03 01:40:05 +0300

    Add urlpatterns for users views

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from users.apps import UsersConfig
+from users.views import RegistrationView, LoginView, LogoutView, UserDetailView, UserUpdateView, UserDeleteView
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('registration/', RegistrationView.as_view(), name='user_registration'),
+    path('login/', LoginView.as_view(), name='user_login'),
+    path('logout/', LogoutView.as_view(), name='user_logout'),
+    path('<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('update/<int:pk>/', UserUpdateView.as_view(), name='user_update'),
+    path('delete/<int:pk>/', UserDeleteView.as_view(), name='user_delete'),
+]

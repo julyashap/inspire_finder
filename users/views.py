5e9b117c3dc2ca30e43ee7a964c8e2726df738d0
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-03 01:45:51 +0300

    Add RegistrationView, LoginView, LogoutView, UserDetailView, UserUpdateView and UserDeleteView users views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.contrib.auth import mixins
+from django.urls import reverse_lazy, reverse
+from django.views import generic
+from users.forms import UserRegistrationForm, UserUpdateForm
+from users.models import User
+from django.contrib.auth.views import LoginView as BaseLoginView, LogoutView as BaseLogoutView
 
-# Create your views here.
+
+class RegistrationView(generic.CreateView):
+    model = User
+    success_url = reverse_lazy('users:user_login')
+    form_class = UserRegistrationForm
+
+
+class LoginView(BaseLoginView):
+    template_name = 'users/user_login.html'
+
+
+class LogoutView(BaseLogoutView):
+    pass
+
+
+class UserDetailView(mixins.LoginRequiredMixin, mixins.UserPassesTestMixin, generic.DetailView):
+    model = User
+
+    def test_func(self):
+        return self.request.user == self.object
+
+
+class UserUpdateView(mixins.LoginRequiredMixin, mixins.UserPassesTestMixin, generic.UpdateView):
+    model = User
+    form_class = UserUpdateForm
+
+    def test_func(self):
+        return self.request.user == self.object
+
+    def get_success_url(self):
+        return reverse('users:user_detail', kwargs={'pk': self.object.pk})
+
+
+class UserDeleteView(mixins.LoginRequiredMixin, mixins.UserPassesTestMixin, generic.DeleteView):
+    model = User
+    success_url = reverse_lazy('recommendations:item_list')
+
+    def test_func(self):
+        return self.request.user == self.object

7361be5bce014205cd042aaaa65ad11b9d834a08
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-03 14:02:07 +0300

    Add clean methods for name and description fields in ItemForm of Item model

--- recommendations/forms.py
+++ recommendations/forms.py
@@ -3,6 +3,27 @@ from recommendations.models import Item
 
 
 class ItemForm(forms.ModelForm):
+    stop_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар',
+                  'реклама', 'ставки', 'кредит', 'займ', 'долг', 'алкоголь', 'наркотики', 'порнография']
+
     class Meta:
         model = Item
         fields = ('name', 'description', 'picture',)
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data.get('name')
+
+        for stop_word in self.stop_words:
+            if stop_word in cleaned_data.lower() or cleaned_data.lower() in stop_word:
+                raise forms.ValidationError('Такое название недопустимо!')
+
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data.get('description')
+
+        for stop_word in self.stop_words:
+            if stop_word in cleaned_data.lower() or cleaned_data.lower() in stop_word:
+                raise forms.ValidationError('Такое описание недопустимо!')
+
+        return cleaned_data

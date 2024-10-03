61c6994cdc7dd5df2082c446f19bc9ce62aea932
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-03 14:03:45 +0300

    Add clean methods for phone field in UserRegistrationForm and UserUpdateForm of User model and also add help_text parameter for phone field

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+import re
 from django.contrib.auth import forms
 from users.models import User
 from django.forms import HiddenInput
@@ -8,13 +9,35 @@ class UserRegistrationForm(forms.UserCreationForm):
         model = User
         fields = ('email', 'phone', 'password1', 'password2',)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['phone'].help_text = 'Номер телефона должен начинаться с "+7" или с "8" и содержать всего 11 цифр.'
+
+    def clean_phone(self):
+        cleaned_data = self.cleaned_data.get('phone')
+
+        if not re.match(r'^(8\d{10}|\+7\d{10})$', cleaned_data):
+            raise forms.ValidationError('Номер телефона должен начинаться с "+7" или "8" и содержать далее 10 цифр!')
+
+        return cleaned_data
+
 
 class UserUpdateForm(forms.UserChangeForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'phone', 'avatar', 'country',)
+        fields = ('first_name', 'last_name', 'email', 'phone', 'avatar', 'city',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.fields['password'].widget = HiddenInput()
+        self.fields['phone'].help_text = 'Номер телефона должен начинаться с "+7" или с "8" и содержать всего 11 цифр.'
+
+    def clean_phone(self):
+        cleaned_data = self.cleaned_data.get('phone')
+
+        if not re.match(r'^(8\d{10}|\+7\d{10})$', cleaned_data):
+            raise forms.ValidationError('Номер телефона должен начинаться с "+7" или "8" и содержать далее 10 цифр!')
+
+        return cleaned_data

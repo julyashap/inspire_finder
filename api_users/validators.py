b95bf4a59ae6a9ff374888b750184e89c641d800
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-06 02:40:23 +0400

    Add PhoneValidator for UserSerializer field

---
+++ api_users/validators.py
@@ -0,0 +1,10 @@
+import re
+from rest_framework.exceptions import ValidationError
+
+
+class PhoneValidator:
+    def __call__(self, value):
+        phone = dict(value).get('phone')
+
+        if not re.match(r'^(8\d{10}|\+7\d{10})$', phone):
+            raise ValidationError('Номер телефона должен начинаться с "+7" или "8" и содержать далее 10 цифр!')

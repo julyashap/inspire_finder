53f8a1d7737d8ebbc4800766c5139927fb05ac81
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-06 02:38:43 +0400

    Add UserCreateAPIView, UserUpdateAPIView, UserRetrieveAPIView and UserDestroyAPIView

--- api_users/views.py
+++ api_users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from api_users.permissions import IsCurrentUser
+from api_users.serializers import UserSerializer
+from users.models import User
 
-# Create your views here.
+
+class UserCreateAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsCurrentUser]
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsCurrentUser]
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsCurrentUser]

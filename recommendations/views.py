ffbba0ce6858a6926a51eaee81efd0761cb90e4e
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-06 18:13:56 +0400

    Fix views with new fields in Item and Like model (UserItemListView, UserLikeListView, ItemListView, ItemCreateView, ItemUpdateView, like_item, unlike_item, RecommendedItemView)

--- recommendations/views.py
+++ recommendations/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy, reverse
 from django.views import generic
 from recommendations.forms import ItemForm
 from recommendations.models import Item, Like
-from recommendations.services import collaborative_filtering_alg
+from recommendations.services import collaborative_filtering_alg, NOW
 
 
 class UserItemListView(mixins.LoginRequiredMixin, generic.ListView):
@@ -13,7 +13,7 @@ class UserItemListView(mixins.LoginRequiredMixin, generic.ListView):
     template_name = 'recommendations/user_item_list.html'
 
     def get_queryset(self):
-        self.queryset = Item.objects.filter(user=self.request.user)
+        self.queryset = Item.objects.filter(user=self.request.user).order_by('-created_at')
         return super().get_queryset()
 
 
@@ -22,7 +22,11 @@ class UserLikeListView(mixins.LoginRequiredMixin, generic.ListView):
     template_name = 'recommendations/user_like_list.html'
 
     def get_queryset(self):
-        self.queryset = Item.objects.filter(like__user=self.request.user)
+        user_likes = Like.objects.filter(user=self.request.user).order_by('-created_at').\
+            values_list('item_id', flat=True)
+
+        self.queryset = Item.objects.filter(pk__in=user_likes)
+
         return super().get_queryset()
 
 
@@ -35,11 +39,11 @@ class ItemListView(generic.ListView):
         if self.request.user.is_authenticated:
             if query:
                 self.queryset = Item.objects.filter(name__icontains=query).\
-                    exclude(user=self.request.user)
+                    exclude(user=self.request.user).order_by('-count_likes')
             else:
-                self.queryset = Item.objects.exclude(user=self.request.user)
+                self.queryset = Item.objects.exclude(user=self.request.user).order_by('-count_likes')
         elif query:
-            self.queryset = Item.objects.filter(name__icontains=query)
+            self.queryset = Item.objects.filter(name__icontains=query).order_by('-count_likes')
 
         return super().get_queryset()
 
@@ -62,6 +66,7 @@ class ItemCreateView(mixins.LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         item = form.save()
         item.user = self.request.user
+        item.created_at = NOW
         item.save()
         return super().form_valid(form)
 
@@ -72,6 +77,12 @@ class ItemUpdateView(mixins.LoginRequiredMixin, mixins.UserPassesTestMixin, gene
 
     def test_func(self):
         return self.request.user == self.object.user
+
+    def form_valid(self, form):
+        item = form.save()
+        item.updated_at = NOW
+        item.save()
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('recommendations:item_detail', kwargs={'pk': self.object.pk})
@@ -97,6 +108,10 @@ def like_item(request, pk):
         return redirect(reverse('recommendations:like_error'))
 
     like = Like.objects.create(user=request.user, item=item)
+    like.created_at = NOW
+    item.count_likes += 1
+
+    item.save()
     like.save()
 
     return redirect(reverse('recommendations:item_list'))
@@ -107,6 +122,10 @@ def unlike_item(request, pk):
     item = Item.objects.get(pk=pk)
     like = Like.objects.filter(user=request.user, item=item)
     like.delete()
+
+    item.count_likes -= 1
+    item.save()
+
     return redirect(reverse('recommendations:item_list'))
 
 
@@ -117,7 +136,7 @@ class RecommendedItemView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
 
         recommended_items_ids = collaborative_filtering_alg(self.request.user.pk)
-        recommended_items = Item.objects.filter(pk__in=recommended_items_ids)
+        recommended_items = Item.objects.filter(pk__in=recommended_items_ids).order_by('-count_likes')
 
         context['object_list'] = recommended_items
         context['user_likes_list'] = Like.objects.filter(user=self.request.user).values_list('item_id', flat=True)

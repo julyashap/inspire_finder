88c2f2abf430c1e9f9bb972922fb84cb8d82d65d
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-06 18:10:37 +0400

    Add count_likes, created_at and updates_at fields to Item model and add created_at field to Like model

--- recommendations/models.py
+++ recommendations/models.py
@@ -11,6 +11,9 @@ class Item(models.Model):
     description = models.TextField(verbose_name='описание', **NULLABLE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='создатель', **NULLABLE)
     picture = models.ImageField(upload_to='recommendations/', verbose_name='изображение', **NULLABLE)
+    count_likes = models.IntegerField(verbose_name='количество лайков', default=0)
+    created_at = models.DateTimeField(verbose_name='дата создания', **NULLABLE)
+    updated_at = models.DateTimeField(verbose_name='дата последнего обновления', **NULLABLE)
 
     def __str__(self):
         return f'item {self.name}'
@@ -25,6 +28,7 @@ class Like(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь')
     item = models.ForeignKey(Item, on_delete=models.CASCADE, verbose_name='элемент')
+    created_at = models.DateTimeField(verbose_name='дата создания', **NULLABLE)
 
     def __str__(self):
         return f'like from {self.user} to {self.item}'

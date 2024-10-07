7634fef0853426ed200efc1bd8971678a7bc7950
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-07 17:18:35 +0400

    Create model Category and add fields category, is_published to Item model

--- recommendations/models.py
+++ recommendations/models.py
@@ -4,16 +4,33 @@ from config import settings
 NULLABLE = {'null': True, 'blank': True}
 
 
+class Category(models.Model):
+    """Модель категории элементов в системе"""
+
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'category {self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
 class Item(models.Model):
     """Модель элемента в системе"""
 
     name = models.CharField(max_length=100, verbose_name='название')
     description = models.TextField(verbose_name='описание', **NULLABLE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='создатель', **NULLABLE)
     picture = models.ImageField(upload_to='recommendations/', verbose_name='изображение', **NULLABLE)
     count_likes = models.IntegerField(verbose_name='количество лайков', default=0)
     created_at = models.DateTimeField(verbose_name='дата создания', **NULLABLE)
     updated_at = models.DateTimeField(verbose_name='дата последнего обновления', **NULLABLE)
+    is_published = models.BooleanField(verbose_name='признак публикации', default=False)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='создатель', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория', **NULLABLE)
 
     def __str__(self):
         return f'item {self.name}'

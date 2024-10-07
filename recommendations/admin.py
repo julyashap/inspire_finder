bf7339b94bc90fc15654821b521f34215d7c27de
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-07 20:08:44 +0400

    Add Item, Like and Category models to admin

--- recommendations/admin.py
+++ recommendations/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from recommendations.models import Item, Category, Like
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description',)
+    search_fields = ('name', 'description',)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description', 'count_likes', 'created_at', 'updated_at', 'is_published',
+                    'user', 'category',)
+    search_fields = ('name', 'description',)
+    list_filter = ('is_published',)
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'item', 'created_at',)

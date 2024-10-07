bbbbf3ee20f9f551d1c5ba1895c69ecbc5386f86
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-07 21:41:08 +0400

    Add phone_confirm to urlpatterns

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import RegistrationView, LoginView, LogoutView, UserDetailView, UserUpdateView, UserDeleteView
+from users.views import RegistrationView, LoginView, LogoutView, UserDetailView, UserUpdateView, UserDeleteView, \
+    phone_confirm
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='user_registration'),
+    path('phone-confirm/', phone_confirm, name='phone_confirm'),
     path('login/', LoginView.as_view(), name='user_login'),
     path('logout/', LogoutView.as_view(), name='user_logout'),
 

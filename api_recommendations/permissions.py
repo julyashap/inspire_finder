1b7f98d3f17aee1f5acc22bd3d15c3ef3f1ea2fe
Julya Shapaeva <shapaeva03@mail.ru>
2024-10-09 03:55:14 +0400

    Add DoesHaveLikes permission

--- api_recommendations/permissions.py
+++ api_recommendations/permissions.py
@@ -4,3 +4,8 @@ from rest_framework import permissions
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user
+
+
+class DoesHaveLikes(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.like_set.exists()
